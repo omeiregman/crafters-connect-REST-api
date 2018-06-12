dd2af78a837f556d6dc8e053d813b38f95c07993
omeiregman <omeiregodsman@gmail.com>
2018-06-12 14:18:51 +0100

    authentication

---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const dotenv = require("dotenv").config();
+const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+
+const userRoutes = require('./api/routes/user');
+
+mongoose.connect('mongodb://craftersweb:'+ process.env.MONGO_MLAB_PW +'@ds137600.mlab.com:37600/crafterswebapi');
+
+
+app.use(morgan('dev'));
+//app.use('/uploads', express.static('uploads'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+
+//FIX CORS
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json({});
+      }
+  next();
+});
+
+//Routes handling requests
+app.use("/user", userRoutes);
+
+
+app.use((req, res, next) => {
+  const error = new Error('Not Found');
+  error.status = 404;
+  next(error);
+})
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  })
+});
+
+module.exports = app;
